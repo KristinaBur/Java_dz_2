8aa03d742b4a3220a21e1eeb3e57e7b6b73e0b16
«Kristina» <kristinka007_92@mail.ru>
2023-04-20 10:48:22 +0300

    Задача 2

--- Task_2.java
+++ Task_2.java
@@ -9,10 +9,30 @@
 //return "";
 
 
-
+import java.util.Scanner;
 
 public class Task_2 {
     public static void main(String[] args) {
-        
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Введите строку: "); 
+        String S = sc.nextLine();
+        System.out.println("Введите массив индексов через запятую(1, 2, 3): ");
+        String num = sc.nextLine();
+        sc.close();
+        String StrArr[] = num.split(", ");
+        int[] index = new int[StrArr.length];
+        for (int i = 0; i < StrArr.length; i++) {
+            index[i] = Integer.parseInt(StrArr[i]);
     }
+        System.out.print(shuffle(S, index)); //shuffle перетасовка       
+}
+
+    public static String shuffle(final String S, final int[] index) {
+        String result = "";
+        for (int i = 0; i < index.length; i++) {
+        int j = index[i];
+        result += S.charAt(j - 1); //charAt возвращает символ, расположенный по указанному индексу строки
+    }
+        return result;
+}
 }
